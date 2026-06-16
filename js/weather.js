fe15d4f7f2233a7658a1744728b5e676b49c2ea4
user <37504497@qq.com>
2026-06-16 23:33:44 +0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,16 @@
 // 天气预报
-async function getRealtimeWeather() {
-  try {
-    const res = await axios({
-      url: 'http://hmajax.itheima.net/api/weather',
-      method: 'get',
-      params: {
-        city: '110100'
-      }
-    })
-    console.log(res);
-    let html = `<h2>${res.data.data.area}未来7日天气预报</h2>`
-    for (let i = 0; i < res.data.data.dayForecast.length; i++) {
-      const item = res.data.data.dayForecast[i];
-      html += `
+axios({
+  url: 'http://hmajax.itheima.net/api/weather',
+  method: 'get',
+  params: {
+    city: '110100'
+  }
+}).then(res => {
+  console.log(res);
+  let html = `<h2>${res.data.data.area}未来7日天气预报</h2>`
+  for (let i = 0; i < res.data.data.dayForecast.length; i++) {
+    const item = res.data.data.dayForecast[i];
+    html += `
       <div class="weather-card">
         <div class="date">日期：${item.date}</div>
         <div class="temperature">温度：${item.temNight}°C~${item.temDay}°C</div>
@@ -21,11 +19,6 @@ async function getRealtimeWeather() {
         <div class="wind-power">风力：${item.windPower}</div>
       </div>
       `
-    }
-    document.querySelector('.content').innerHTML = html;
-  } catch (error) {
-    console.error('获取天气数据失败：', error);
-    document.querySelector('.content').innerHTML = '获取失败';
   }
-}
-getRealtimeWeather();
+  document.querySelector('.content').innerHTML = html;
+})
