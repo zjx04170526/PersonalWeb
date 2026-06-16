7fa13310642a3785a9c0fa8130dcaa5588779641
user <37504497@qq.com>
2026-06-16 23:26:12 +0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,34 +1,31 @@
 // 天气预报
 async function getRealtimeWeather() {
-  const apiKey = 'c63f38c44bb144f4b05c3978c7efe5be';
-  const location = '101010100';
-  const url = `https://n233jnfyt6.re.qweatherapi.com/v7/weather/7d?location=${location}&key=${apiKey}`
   try {
     const res = await axios({
+      url: 'http://hmajax.itheima.net/api/weather',
       method: 'get',
-      url: url,
+      params: {
+        city: '110100'
+      }
     })
-    console.log(res.data.daily);
-    let html = `<h2>北京未来7日天气预报</h2>`
-    for (let i = 0; i < res.data.daily.length; i++) {
-      const item = res.data.daily[i];
+    console.log(res);
+    let html = `<h2>${res.data.data.area}未来7日天气预报</h2>`
+    for (let i = 0; i < res.data.data.dayForecast.length; i++) {
+      const item = res.data.data.dayForecast[i];
       html += `
       <div class="weather-card">
-        <div class="date">日期：${item.fxDate}</div>
-        <div class="temperature">温度：${item.tempMin}°C~${item.tempMax}°C</div>
-        <div class="weather">天气：${item.textDay}</div>
-        <div class="vis">能见度：${item.vis}km</div>
-        <div class="wind-direction">风向：${item.windDirDay}</div>
-        <div class="wind-scale">风力：${item.windScaleDay}级</div>
-        <div class="wind-speed">风速：${item.windSpeedDay}km/h</div>
+        <div class="date">日期：${item.date}</div>
+        <div class="temperature">温度：${item.temNight}°C~${item.temDay}°C</div>
+        <div class="weather">天气：<img src="${item.weatherImg}" alt="${item.weather}"> ${item.weather}</div>
+        <div class="wind-direction">风向：${item.windDirection}</div>
+        <div class="wind-power">风力：${item.windPower}</div>
       </div>
       `
     }
     document.querySelector('.content').innerHTML = html;
   } catch (error) {
     console.error('获取天气数据失败：', error);
-    document.querySelector('.temperature').innerHTML = '获取失败';
-    document.querySelector('.weather').innerHTML = '获取失败';
+    document.querySelector('.content').innerHTML = '获取失败';
   }
 }
 getRealtimeWeather();
